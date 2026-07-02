3b8f553cb0b3dee7588f503a51be11e5e3fbf447
Pathum Jayasundara <pathumjayasundara04@gmail.com>
2026-07-02 19:36:19 +0530

    Remove unnecessary partial class definition

--- 22APP6134 Pathum Jayasundara/MortgageCalculator/Form1.Designer.cs
+++ 22APP6134 Pathum Jayasundara/MortgageCalculator/Form1.Designer.cs
@@ -1,10 +1,4 @@
 
-    partial class Form1
-    {
-        private System.ComponentModel.IContainer components = null;
-
-        protected override void Dispose(bool disposing)
-        {
             if (disposing && (components != null))
                 components.Dispose();
             base.Dispose(disposing);
